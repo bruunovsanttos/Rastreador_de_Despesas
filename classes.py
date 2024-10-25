f29593dd8450bc52e90c5e928d65e0de72cf6f8a
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2024-10-25 12:47:29 -0300

    ajuste de datetime para printe da hora

--- classes.py
+++ classes.py
@@ -1,12 +1,18 @@
+import datetime
+
+
 class Despesa:
     def __init__(self, id, data, descricao, valor, categoria=None):
-    self.id = id
-    self.data = data 
-    self.descricao = descricao
-    sel.valor = valor
-    self.categoria = categoria
+        self.id = id
+        self.data = datetime.now()strftime('d%-m%-Y%')
+        self.descricao = descricao
+        self.valor = valor
+        self.categoria = categoria
 
     def __str__(self):
         return f"{self.id} - {self.data} - {self.descricao} - R${self.valor:.2f} - Categoria: {self.categoria or 'Não definida'}"
+
+
+    
 
 
