eca1a34635c755acda72edc8c672358439a9730a
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2024-10-25 12:55:12 -0300

    adicionando tarefas do rastreador de despesas

--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ import datetime
 
 
 class Despesa:
-    def __init__(self, id, data, descricao, valor, categoria=None):
+    def __init__(self, id, descricao, valor, categoria=None):
         self.id = id
         self.data = datetime.now()strftime('d%-m%-Y%')
         self.descricao = descricao
@@ -13,6 +13,31 @@ class Despesa:
         return f"{self.id} - {self.data} - {self.descricao} - R${self.valor:.2f} - Categoria: {self.categoria or 'Não definida'}"
 
 
+class RastreadorDeDespesas:
+
+    def __init__(self):
+        self.despesas = []
+
+    def CarregarDespesas(self): #abreo json
+        pass
+
+    def SalvarDesperas(self): #salva o json
+        pass
+
+    def AdicionarDespesas(self):
+        pass
+
+    def MostrarDespesas(self):
+        pass
+
+    def ExcluirDespesas(self):
+        pass
+
     
+
+
+
+
+
 
 
